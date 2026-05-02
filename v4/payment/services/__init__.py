fc9a27e9146b8fd10f8eea4d877319631bbb07dc
suzuki-tomok <suzuki.tomok@outlook.jp>
2026-05-02 20:46:13 +0900

    docs: services/__init__.py の import 例を実在する exception に修正

--- v4/payment/services/__init__.py
+++ v4/payment/services/__init__.py
@@ -3,5 +3,5 @@
 caller (view 等) はここの submodule から直接 import する:
     from payment.services.checkout import create_checkout_url
     from payment.services.dtos import CheckoutInput
-    from payment.services.exceptions import DuplicateOrderError
+    from payment.services.exceptions import InvalidInputError
 """
